ce72f0c4d08ee00143b86b16897e8027ccb08003
Shadab Siddiqui <shadab.siddiqui@Infraseepz.lan>
2021-04-27 00:58:11 +0530

    Final v0.1 as per my understanding

--- test/test-bmi.js
+++ test/test-bmi.js
@@ -4,10 +4,10 @@ let bmiCalculator = require('../bmi-calculator');
 describe('bmiCalculator', async function () {
     console.log("result");
     it('should return Bmi Json', async function () {
-      var preResult = '[{"Gender": "Male", "HeightCm": 171, "WeightKg": 96 }, { "Gender": "Male", "HeightCm": 161, "WeightKg": 85 }, { "Gender": "Male", "HeightCm": 180, "WeightKg": 77 }, { "Gender": "Female", "HeightCm": 166, "WeightKg": 62}, {"Gender": "Female", "HeightCm": 150, "WeightKg": 70}, {"Gender": "Female",  "HeightCm": 167, "WeightKg": 82}]';
+      var preResult = {Malnutritionrisk: 0,Lowrisk: 0,Enhancedrisk:0,Mediumrisk:0,Highrisk:1,Veryhighrisk:5,};
       var result = await bmiCalculator.calculateBmiBasedOnJson(`[{"Gender": "Male", "HeightCm": 171, "WeightKg": 96 }, { "Gender": "Male", "HeightCm": 161, "WeightKg": 85 }, { "Gender": "Male", "HeightCm": 180, "WeightKg": 77 }, { "Gender": "Female", "HeightCm": 166, "WeightKg": 62}, {"Gender": "Female", "HeightCm": 150, "WeightKg": 70}, {"Gender": "Female",  "HeightCm": 167, "WeightKg": 82}]`);
       console.log(result);
-      expect(result).to.be.equal(preResult);
+      expect(JSON.stringify(result)).to.be.equal(JSON.stringify(preResult));
   
     });
 });
